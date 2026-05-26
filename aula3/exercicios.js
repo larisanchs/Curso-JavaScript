7852ea335cfe2123791837bfb676232ba7468fbf
larisanchs <larissasanches952@gmail.com>
2026-05-25 21:50:38 -0300

    exercícios aula 4

--- aula3/exercicios.js
+++ aula3/exercicios.js
@@ -15,10 +15,10 @@ let dia = 1
 switch (dia){
     case(1): console.log("Domingo"); break;
     case(2): console.log("Segunda-feira"); break;
-    case(3): console.log("Terca-feira"); break;
+    case(3): console.log("Terça-feira"); break;
     case(4): console.log("Quarta-feira"); break;
     case(5): console.log("Quinta-feira"); break;
     case(6): console.log("Sexta-feira"); break;
-    case(7): console.log("Sabado-feira"); break;
+    case(7): console.log("Sábado"); break;
     default: console.log("Dia legal")
 }
---
+++ aula4/exercicios.js
@@ -0,0 +1,15 @@
+// 1. Use um laço for para exibir os números de 1 a 10.
+for (let i=0; i<=10; i++){
+    console.log(i);
+}
+
+// 2. Use while para somar números de 1 a 5.
+var j = 0
+while (j<=5){
+    let soma = j+1
+    console.log(`A soma de ${j} + 1 é igual a: ${soma}`)
+    j++
+}
+
+// 3. Crie um do...while que peça uma senha até que seja '1234'.
+
